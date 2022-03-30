e30e2d4a799c985daaa39cc43f5b770ac21f1b8e
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-30 15:21:08 -0400

    changed styles for consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles["navbar"]}>
       <ul>
         <li className={styles.title}>
           <Link to="/">qMoney Tracker</Link>
