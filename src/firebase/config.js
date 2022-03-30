794ba9172431ad8631a9317ebee55415b5f41e55
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-30 15:21:19 -0400

    added config for firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtKMuWErtia_YrFResnzIAoe0OwNBJ7e4",
+  authDomain: "qmoney-tracker.firebaseapp.com",
+  projectId: "qmoney-tracker",
+  storageBucket: "qmoney-tracker.appspot.com",
+  messagingSenderId: "569542886110",
+  appId: "1:569542886110:web:7373ed00b26a4b9f60b84f",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth()
+
+export { db, auth };
