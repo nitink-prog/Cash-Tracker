1d4f35bfa400c9af47c946a710b48662699793ab
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-30 15:21:53 -0400

    created Signup form and styled it

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,44 @@
-import styles from "./Signup.module.css"
+import { useState } from "react";
+import styles from "./Signup.module.css";
 
 export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, name);
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      <h2>Create Your Account</h2>
+      <label>
+        <span>Name:</span>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
