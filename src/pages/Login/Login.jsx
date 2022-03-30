99ce6933ac09e68e92fbe828b3c35cd1aa2073dc
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-30 12:09:20 -0400

    rudimentary logic for logging in

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,35 @@
-import styles from "./Login.module.css"
+import { useState } from "react";
+import styles from "./Login.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h2>Your qMoney Account</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Log in</button>
+    </form>
+  );
 }
