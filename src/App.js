538fc39975b45e686fce79deb418b9990d9f45ef
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-30 11:46:06 -0400

    added some pages and routed them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
