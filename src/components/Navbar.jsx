305b293df769097c5aa7dae9a92a3c0af4ef64e0
Nitin K <80429955+nitink-prog@users.noreply.github.com>
2022-04-04 19:56:30 -0400

    working on logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles["navbar"]}>
       <ul>
@@ -11,6 +14,9 @@ export default function Navbar() {
         <li>
           <Link to="/login">Log in</Link>
           <Link to="/signup">Sign up</Link>
+        </li>
+        <li className="btn" onClick={logout}>
+          Log out
         </li>
       </ul>
     </nav>
