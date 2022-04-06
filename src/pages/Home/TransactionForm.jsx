9b30fe7abef60d0dd3d48bcd0145f33cef98366d
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-06 15:56:33 -0400

    adding document with uid

--- src/pages/Home/TransactionForm.jsx
+++ src/pages/Home/TransactionForm.jsx
@@ -1,14 +1,23 @@
 // component only used in <Home />
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function TransactionForm() {
+export default function TransactionForm({ uid }) {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const { addDocument, response } = useFirestore("transactions");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ name, amount });
+    addDocument({ uid, name, amount });
   };
+
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
 
   return (
     <>
