e1d0d5cca26080f0e22a4e07f3fd5ae212998687
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-06 15:55:14 -0400

    adding documents and error handling

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useReducer, useEffect, useState } from "react";
-import { db } from "../firebase/config";
+import { db, timestamp } from "../firebase/config";
 
 let initialState = {
   document: null,
@@ -11,7 +11,29 @@ let initialState = {
 const firestoreReducer = (state, action) => {
   switch (action.type) {
     case "IS_PENDING":
-      return { ...state, isPending: true };
+      return {
+        ...state,
+        document: null,
+        isPending: true,
+        error: null,
+        success: false,
+      };
+    case "ADDED_DOC":
+      return {
+        ...state,
+        document: action.payload,
+        isPending: false,
+        error: null,
+        success: true,
+      };
+    case "ERROR":
+      return {
+        ...state,
+        isPending: false,
+        document: null,
+        error: action.payload,
+        success: false,
+      };
     default:
       return state;
   }
@@ -24,12 +46,21 @@ export const useFirestore = (collection) => {
   // collection ref
   const ref = db.collection(collection);
 
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   const addDocument = async (doc) => {
     dispatch({ type: "IS_PENDING" });
     try {
-        const addedDoc = await ref.add(doc)
-        
-    } catch (err) {}
+      const createdAt = timestamp.fromDate(new Date());
+      const addedDoc = await ref.add({ ...doc, createdAt });
+      dispatchIfNotCancelled({ type: "ADDED_DOC", payload: addedDoc });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
   };
 
   const deleteDocument = async (id) => {
