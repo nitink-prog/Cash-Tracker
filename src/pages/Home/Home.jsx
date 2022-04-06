77c05421aea9d9b746326aeb08329dec3968070f
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-06 15:55:31 -0400

    pass uid as prop

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
-import styles from "./Home.module.css";
 import TransactionForm from "./TransactionForm";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import styles from "./Home.module.css";
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles["container"]}>
       <div className={styles["content"]}>Transaction List</div>
       <div className={styles["sidebar"]}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
