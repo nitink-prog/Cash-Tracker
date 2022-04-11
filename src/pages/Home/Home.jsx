de93349582436ee9c062361afb13e85dc67768ea
Nitin K <80429955+nitink-prog@users.noreply.github.com>
2022-04-11 19:34:11 -0400

    displaying Transaction List component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,19 @@
-import TransactionForm from "./TransactionForm";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 import styles from "./Home.module.css";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions", ["uid", "==", user.uid]);
 
   return (
     <div className={styles["container"]}>
-      <div className={styles["content"]}>Transaction List</div>
+      <div className={styles["content"]}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles["sidebar"]}>
         <TransactionForm uid={user.uid} />
       </div>
